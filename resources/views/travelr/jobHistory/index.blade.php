9aac981726fcf539a77c3f9e39268fddf5a9bbb8
Fakhar <fakhar.imran008@gmail.com>
2024-07-18 05:38:04 +0500

    fkr

--- resources/views/travelr/jobHistory/index.blade.php
+++ resources/views/travelr/jobHistory/index.blade.php
@@ -1,303 +1,120 @@
 @extends('layouts.app')
 
 @section("top_links")
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css">
-{{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> --}}
+<link rel="stylesheet" href ="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css">
 @endsection
 
 @section("bottom_links")
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
 <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+
 <script>
-  
-
-    // $(document).ready(function() {
-    //   var table = $('#customDataTable').DataTable({
-    //     // Add your DataTable options here, if needed.
-    //   });
-    
-    //   // Add a custom button
-    //   $('<button class="btn btn-primary btn-sm">Custom Button</button>')
-    //     .appendTo($('#customDataTable_wrapper .dataTables_filter'))
-    //     .on('click', function() {
-    //       // Custom button action here
-    //       alert('Custom button clicked!');
-    //     });
-    
-    //   // Add a search bar (custom filter)
-    //   $('#customDataTable_filter input').unbind().bind('keyup', function() {
-    //     table.search(this.value).draw();
-    //   });
-    // });
-
-
-//     $(document).ready(function() {
-//   var table = $('#customDataTable').DataTable({
-//     // Add your DataTable options here, if needed.
-//     "pagingType": "full_numbers" // Use full_numbers paging type for more control
-//   });
-
-
-// });
-
 $(document).ready(function() {
-    var table = $('#customDataTable').DataTable({
-    // Add your custom options here
-    scrollX: true, // scroll horizontally
-    paging: true, // Enable pagination
-    searching: true, // Enable search bar
-    ordering: true, // Enable column sorting
-    lengthChange: true, // Show a dropdown for changing the number of records shown per page
-    pageLength: 10, // Set the default number of records shown per page to 10
-    dom: 'lBfrtip', // Define the layout of DataTable elements (optional)
-    buttons: ['copy', 'excel', 'pdf', 'print'], // Add some custom buttons (optional)
-    "pagingType": "full_numbers"
+    $('#customDataTable').DataTable({
+        "responsive": true,
+        "paging": true,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "columnDefs": [
+            { "orderable": false, "targets": -1 } // Disable ordering on the last column (Send Button)
+        ]
+    });
 
-    
-  });
-//   $('.table_btn_list').appendTo(table.table().container());
+    // Add filters for each column
+    $('#customDataTable thead tr').clone(true).appendTo( '#customDataTable thead' );
+    $('#customDataTable thead tr:eq(1) th').each( function (i) {
+        var title = $(this).text();
+        if (title !== '#' && title !== 'Send Button') { // Exclude columns that don't need filters
+            $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+            $( 'input', this ).on( 'keyup change clear', function () {
+                if ( table.column(i).search() !== this.value ) {
+                    table
+                        .column(i)
+                        .search( this.value )
+                        .draw();
+                }
+            } );
+        } else {
+            $(this).html('');
+        }
+    });
 
-  $('#customSearchInput').on('keyup', function() {
-    table.search(this.value).draw();
-  });
-
-  // Custom search input for 'Name' column
-  $('#name-search').on('change', function() {
-    table.column(1).search(this.value).draw();
-  });
-
-  $('#next').on('click', function() {
-      if (table.page() < table.pages() - 1) {
-        customPagination(table.page() + 1);
-      }
-   });
-   $('#previous').on('click', function() {
-    if (table.page() > 0) {
-        customPagination(table.page() - 1);
-      }
-   });
-
-
-    
-  var $paginationContainer = $('.custom-pagination');
-
-function goToPage(pageNumber) {
-  table.page(pageNumber).draw('page');
-  updatePaginationButtons();
-}
-
-function updatePaginationButtons() {
-  var currentPage = table.page();
-  var totalPages = table.page.info().pages;
-  var pageInfo = table.page.info();
-
-  $paginationContainer.empty();
-
-  if (currentPage >= 0) {
-    $paginationContainer.append(`<img class=" clickable-element next-prev-icon-style custom-page-btn prev" src="{{asset('assets/images/privious.png')}}"> ${pageInfo.start + 1} `);
-  }
-
-//   for (var i = 0; i < totalPages; i++) {
-//     var activeClass = currentPage === i ? 'active' : '';
-//     $paginationContainer.append('<button class="custom-page-btn ' + activeClass + '">' + (i + 1) + '</button>');
-//   }
-
-  if (currentPage <= totalPages - 1) {
-    $paginationContainer.append(` ${pageInfo.end} <img  class=" clickable-element next-prev-icon-style custom-page-btn next"  src="{{asset('assets/images/next.png')}}">`);
-  }
-
-  $('.custom-page-btn').on('click', function() {
-    if ($(this).hasClass('prev')) {
-      goToPage(currentPage - 1);
-    } else if ($(this).hasClass('next')) {
-      goToPage(currentPage + 1);
-    } else {
-      goToPage(parseInt($(this).text()) - 1);
-    }
-  });
-}
-
-updatePaginationButtons();
-
-
-
-    // Update the custom button text with the current page length
-  $('.current_pages').text(table.page.len());
-  
-  // Handle custom button click (Dropdown item selection)
-  $('.dropdown-item').on('click', function() {
-    var length = $(this).data('length');
-    table.page.len(length).draw();
-    updatePaginationButtons();
-
-    // Update the custom button text with the selected page length
-    $('.current_pages').text(length);
-  });
-
-
-
-
-
-  function updateTableInfo() {
-    var pageInfo = table.page.info();
-    var infoText = `${pageInfo.recordsTotal} records in total`;
-    var startPage = `${pageInfo.start + 1}`;
-    var endPage = `${pageInfo.end}`;
-
-    $('.custom-table-info').text(infoText);
-    $('.start_page').text(startPage);
-    $('.end_page').text(endPage);
-
-
-  }
-//  `Showing ${pageInfo.start + 1} to ${pageInfo.end} of ${pageInfo.recordsTotal} entries`
-  // Call the function to set the initial table information
-  updateTableInfo();
-
-  // Event listener for DataTable page change
-  table.on('page.dt', function() {
-    updateTableInfo();
-  });
-
-
+    var table = $('#customDataTable').DataTable();
 });
 </script>
-
+@endsection
 
 @section('content')
 <div class="container" style="padding:0px">
-
-    <div class="row" style="   max-width: 99%; margin: 1px auto;">
+    <div class="row" style="max-width: 99%; margin: 1px auto;">
         <div class="col-md-12 col-12">
-            <div class="Company" style="text-align: center;" >Dispatch History
-            </div>
-
+            <div class="Company" style="text-align: center;">Dispatch History</div>
         </div>
-        {{-- <div class="col-md-1 col-3"  style="margin: 1px auto;">
-            <div class="add_btn">
-                <a href="{{ route('company.create') }}"> <span>+</span>New</a>
-            </div>
-        </div> --}}
     </div>
     @if (session('success'))
-      <div class="alert alert-success">
-          {{ session('success') }}
-      </div>
-  @endif
-
-  @if (session('error'))
-  <div class="alert alert-danger">
-      {{ session('error') }}
-  </div>
-  @endif
-
-    <div class="row" style="    max-width: 99%; margin: 1px auto; font-size: 12px;">
-
-    <div class="col-12">
-            <div class="table_btn_list">
-
-                <div class="totla_company">
-                    <p class="custom-table-info">10 records in total</p>
-                </div>
-                <div class="pagination_links custom-pagination">
-                    <div class="d-flex clickable-element" id="previous"><img class="next-prev-icon-style" src="{{asset('assets/images/privious.png')}}"><div class="start_page">1</div></div>
-                    <div class="d-flex clickable-element"  id="next"> <div class="end_page"> 10 </div> <img  class="next-prev-icon-style"  src="{{asset('assets/images/next.png')}}"></div>
-                </div>
-            
-                <div class="totla_num clickable-element">
-                    <div class="custom-button">
-                        <a class=" dropdown-toggle"data-toggle="dropdown">
-                          <span class="current_pages">10</span> 
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                          <div class="dropdown-item"  data-length="10">10</div>
-                          <div class="dropdown-item"  data-length="25">25</div>
-                          <div class="dropdown-item"  data-length="50">50</div>
-                          <div class="dropdown-item"  data-length="100">100</div>
-                        </div>
-                      </div>
-                      
-                </div>
-               
-                <div class="refresh_btn clickable-element" onclick="window.location.reload();">
-                    <a ><img src="assets/images/ref.png"> Refresh </a>
-                </div>
-            
-                <div class="filter_btn clickable-element">
-                    <a href="#"><img src="assets/images/filter.png"> Filter </a>
-                </div>
-            
-                <div class="search_bar">
-                    <div class="search_field">
-                        <input id="customSearchInput" type="text" placeholder="">
-                    </div>
-                    <div class="search_btn">
-                        <a> <img src="assets/images/search.png"> </a>
-                    </div>
-                </div>
-            </div>
+        <div class="alert alert-success">
+            {{ session('success') }}
         </div>
+    @endif
 
-        <div class="col-12" style="margin: 1px auto; ">
-            
-            <table id="customDataTable" class="table  datatable table-bordered table-hover table-responsive nowrap" style="width:100%; font-size: small;">
+    @if (session('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
+    @endif
+
+    <div class="row" style="max-width: 99%; margin: 1px auto; font-size: 12px;">
+        <div class="col-12" style="margin: 1px auto;">
+            <table id="customDataTable" class="table datatable table-bordered table-hover table-responsive nowrap" style="width:100%; font-size: small;">
                 <thead>
                     <tr>
-                    <th class="thclass" scope="col">#</th>
-                    <th class="thclass"  scope="col">Date</th>
-                    <th class="thclass"  scope="col">Time</th>
-                    <th class="thclass"  scope="col">Pick up address</th>
-                    <th class="thclass"  scope="col">Drop off Address</th>
-                    <th class="thclass"  scope="col">Phone Number</th>
-                    <th class="thclass"  scope="col">Fare</th>
-                    <th class="thclass"  scope="col">Send Button</th>
-                    <th class="thclass"  scope="col">Time away</th>
-                    <th class="thclass"  scope="col">Job status</th>
+                        <th class="thclass" scope="col">#</th>
+                        <th class="thclass" scope="col">Date</th>
+                        <th class="thclass" scope="col">Time</th>
+                        <th class="thclass" scope="col">Pick up address</th>
+                        <th class="thclass" scope="col">Drop off Address</th>
+                        <th class="thclass" scope="col">Phone Number</th>
+                        <th class="thclass" scope="col">Fare</th>
+                        <th class="thclass" scope="col">Send Button</th>
+                        <th class="thclass" scope="col">Time away</th>
+                        <th class="thclass" scope="col">Job status</th>
                     </tr>
-
                 </thead>
                 @php
                     $dispatchCount = count($dispatchHistory);
-                    @endphp
+                @endphp
                 <tbody>
-                    @if($dispatchHistory!=null)
-                    @foreach ($dispatchHistory as $dispatch)
-                        <tr>
-                            <td class="tdclass">{{ $dispatchCount}}</td>
+                    @if($dispatchHistory != null)
+                        @foreach ($dispatchHistory as $dispatch)
+                            <tr>
+                                <td class="tdclass">{{ $dispatchCount }}</td>
+                                @php
+                                    $createdTime = new DateTime($dispatch['created_at']);
+                                    $formattedCreatedDate = $createdTime->format("Y-m-d");
+                                    $formattedCreatedTime = $createdTime->format("h:i:s A");
+                                @endphp
+                                <td class="tdclass">{{ $formattedCreatedDate }}</td>
+                                <td class="tdclass">{{ $formattedCreatedTime }}</td>
+                                <td class="tdclass">{{ $dispatch['pick_up_address'] }}</td>
+                                <td class="tdclass">{{ $dispatch['drop_off_address'] }}</td>
+                                <td class="tdclass">{{ $dispatch['phone_no'] }}</td>
+                                <td class="tdclass">{{ $dispatch['fare'] }}</td>
+                                <td class="tdclass">{{ $dispatch['send_button'] }}</td>
+                                <td class="tdclass">{{ $dispatch['time_away'] }}</td>
+                                <td class="status" id="status-{{ $dispatch['id'] }}" style="color: {{ $dispatch['status'] == 'completed' ? 'green' : 'red' }};">
+                                    {{ $dispatch['status'] }}
+                                </td>
+                            </tr>
                             @php
-                                $createdTime = new DateTime($dispatch['created_at']);
-
-                                // Format the DateTime object in 12-hour format
-                                $formattedCreatedDate = $createdTime->format("Y-m-d");
-                                $formattedCreatedTime = $createdTime->format("h:i:s A");
+                                $dispatchCount--;
                             @endphp
-                            <td class="tdclass">{{ $formattedCreatedDate }}</td>
-                            <td class="tdclass">{{ $formattedCreatedTime }}</td>
-                            <td class="tdclass">{{ $dispatch['pick_up_address'] }}</td>
-                            <td class="tdclass">{{ $dispatch['drop_off_address'] }}</td>
-                            <td class="tdclass">{{ $dispatch['phone_no'] }}</td>
-                            <td class="tdclass">{{ $dispatch['fare'] }}</td>
-                            <td class="tdclass"> {{$dispatch['send_button']}}</td>
-
-                            <td class="tdclass">{{ $dispatch['time_away'] }}</td>
-                            <td class="status" id="status-{{ $dispatch['id'] }}" style="color: {{ $dispatch['status'] == 'completed' ? 'green' : 'red' }};">
-                                {{ $dispatch['status'] }}
-                            </td>
-
-                        </tr>
-                        @php
-                            $dispatchCount--;
-                        @endphp
-                    @endforeach
+                        @endforeach
                     @endif
                 </tbody>
             </table>
         </div>
     </div>
 </div>
-
-
 @endsection
